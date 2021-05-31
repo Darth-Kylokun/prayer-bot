2baeebde1306e20666bfc12277993a4102930e16
Darth-Kylokun <ftwdrc@gmail.com>
2021-05-31 18:24:57 -0400

    Switched user to 'darth'

--- database/engine.py
+++ database/engine.py
@@ -6,6 +6,6 @@ from dotenv.main import load_dotenv
 def __setup():
     load_dotenv(".env")
     password = os.environ["PG_PASSWORD"]
-    return create_async_engine(f"postgresql+asyncpg://localhost:5432/tomato?user=postgres&password={password}", echo=False), create_engine(f"postgresql://localhost:5432/tomato?user=postgres&password={password}")
+    return create_async_engine(f"postgresql+asyncpg://localhost:5432/tomato?user=darth&password={password}", echo=False), create_engine(f"postgresql://localhost:5432/tomato?user=darth&password={password}")
 
 async_engine, sync_engine = __setup()
