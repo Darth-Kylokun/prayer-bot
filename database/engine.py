0c605e6a1059ea9abbf01c73d5d73b4155e46708
Darth-Kylokun <ftwdrc@gmail.com>
2021-06-01 13:56:27 -0400

    made username for database a environmental variable

--- database/engine.py
+++ database/engine.py
@@ -5,7 +5,8 @@ from dotenv.main import load_dotenv
 
 def __setup():
     load_dotenv(".env")
+    user = os.environ["PG_USER"]
     password = os.environ["PG_PASSWORD"]
-    return create_async_engine(f"postgresql+asyncpg://localhost:5432/tomato?user=darth&password={password}", echo=False), create_engine(f"postgresql://localhost:5432/tomato?user=darth&password={password}")
+    return create_async_engine(f"postgresql+asyncpg://localhost:5432/tomato?user={user}&password={password}", echo=False), create_engine(f"postgresql://localhost:5432/tomato?user={user}&password={password}")
 
 async_engine, sync_engine = __setup()
